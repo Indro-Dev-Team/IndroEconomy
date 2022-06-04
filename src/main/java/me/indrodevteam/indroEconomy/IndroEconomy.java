615c73518022da27b742d117d060502d1fc12f4c
skyQuiz <91608035+skyQuiz@users.noreply.github.com>
2022-06-04 19:17:00 +1000

    Initial Commit

---
+++ src/main/java/me/indrodevteam/indroEconomy/IndroEconomy.java
@@ -0,0 +1,16 @@
+package me.indrodevteam.indroEconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IndroEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
